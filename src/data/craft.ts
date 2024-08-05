c23d0a7b5b570b95c15b35d96786d33717dc6abd
limchansoo <cslim0527@gmail.com>
2024-08-05 17:04:04 +0900

    Feat: Craft 페이지 추가

---
+++ src/data/craft.ts
@@ -0,0 +1,170 @@
+export const POTION_DATA = [
+  {
+    name: "금옥수",
+    description: "자향초 18개, 월꽃 8개, 오각독초 1개 [마력 +7000]",
+  },
+  {
+    name: "마력회생약",
+    description: "월꽃 4개, 자향초 4개 [마력 +2000]",
+  },
+  {
+    name: "원기회복약",
+    description: "생기초 1개, 자향초 3개 [체력 +1500]",
+  },
+  {
+    name: "음양한빙수",
+    description:
+      "한담초 4개, 음양초 1개, 영로단초 20개, 월꽃 10개, 노을꽃 1개 [마력 +15000]",
+  },
+  {
+    name: "정맥단",
+    description: "미르의꽃 10개, 영지버섯 2개 [체력 +1000 마법보호]",
+  },
+  {
+    name: "회복수",
+    description: "생기초 6개, 영지버섯 2개, 자향초 15개 [체력 10000]",
+  },
+  {
+    name: "회복약",
+    description: "미르의꽃 1개, 영지버섯 1개 [체력 +500 내공 외공회복]",
+  },
+  {
+    name: "회생단",
+    description:
+      "모용꽃 10개, 생기초 9개, 영지버섯 1개, 오각독초 3개, 노을꽃 6개 [체력 +20000, 마력 +20000]",
+  },
+  {
+    name: "각성분낭",
+    description: "기절한 사람을 부활시킨다. 기절한 사람을 보고 약을 먹는다.",
+  },
+  {
+    name: "마력분낭",
+    description: "마력 +100000",
+  },
+  {
+    name: "회복분낭",
+    description: "체력 +100000",
+  },
+  {
+    name: "마비분낭",
+    description: "상대방을 움직이지 못하게 마비 시킨다.",
+  },
+  {
+    name: "철웅환",
+    description: "체력 +1000 공격력 강화",
+  },
+  {
+    name: "백화주",
+    description: "체력 +20000 마비해제",
+  },
+  {
+    name: "치료분낭",
+    description: "중독해제",
+  },
+];
+
+export const WEAPON_DATA = [
+  {
+    name: "삼인검",
+    description: "구리 20개, 철 20개, 최중석 5개, 애매랄드 1개",
+  },
+  {
+    name: "천마검",
+    description: "구리 10개, 철 20개, 회중석 10개, 현철석 5개, 사파이어 1개",
+  },
+  {
+    name: "역날도",
+    description:
+      "철 5개, 회중석 20개, 염마석 10개, 애매랄드 2개, 다이아몬드 1개",
+  },
+  {
+    name: "금룡검",
+    description:
+      "회중석 30개, 염마석 5개, 천마석 5개, 사파이어 2개, 다이아몬드 2개",
+  },
+  {
+    name: "팔척사모",
+    description: "구리 5개, 철 20개, 정광석 10개, 현철석 10개, 루비 1개",
+  },
+  {
+    name: "활인대검",
+    description:
+      "철 20개, 정광석 15개, 현철석 5개, 초마석 1개, 호박 2개, 다이아몬드 1개",
+  },
+  {
+    name: "제룡검",
+    description:
+      "정광석 20개, 현철석 20개, 초마석 2개, 천마석 2개, 호박 2개, 다이아몬드 2개",
+  },
+];
+
+export const HERBS_DATA = [
+  {
+    name: "자향초",
+    description: "독 -2",
+  },
+  {
+    name: "영로단초",
+    description: "마력 +100, 독 +2",
+  },
+  {
+    name: "월꽃",
+    description: "마력 +500, 독 +2",
+  },
+  {
+    name: "한담초",
+    description: "마력 +1000, 독 -3",
+  },
+  {
+    name: "오각독초",
+    description: "마력 +1000, 독 -3",
+  },
+  {
+    name: "음양초",
+    description: "마력 +4000, 독 +1",
+  },
+  {
+    name: "노을꽃",
+    description: "독 -20",
+  },
+  {
+    name: "생기초",
+    description: "체력 +1500, 독 +5",
+  },
+  {
+    name: "미르의꽃",
+    description: "특수 효능 체력 +100",
+  },
+  {
+    name: "모용꽃",
+    description: "마력 +600, 체력 +600, 독 +2",
+  },
+  {
+    name: "영지버섯",
+    description: "체력 +500",
+  },
+  {
+    name: "불로삼",
+    description: "특수기능으로 마력을 한번에 채운다.",
+  },
+  {
+    name: "녹파영초",
+    description: "특수기능으로 걸려있는 모든마법을 해제한다.",
+  },
+  {
+    name: "해동인삼",
+    description: "특수기능으로 마법보호기능이 있다.",
+  },
+  {
+    name: "신선초",
+    description: "특수기능으로 체력을 한번에 전부 채운다.",
+  },
+  {
+    name: "회복초",
+    description: "체력 +8000",
+  },
+  {
+    name: "만년콩",
+    description: "체력 +10000",
+  },
+];
---
+++ src/feat-components/Craft/WeaponTable.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import styled from "styled-components";
+import { ifErrorNoImg } from "@/src/utils/common";
+import { WEAPON_DATA } from "@/src/data/craft";
+
+const WeaponTable = () => {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>이미지</th>
+          <th>이름</th>
+          <th>재료,효능,효과</th>
+        </tr>
+      </thead>
+      <tbody>
+        {WEAPON_DATA.map(({ name, description }, index) => (
+          <tr key={`weapon-row-${index}`}>
+            <td>
+              <img
+                src={`/assets/images/crafts/weapon/${name}.gif`}
+                alt=""
+                onError={ifErrorNoImg}
+              />
+            </td>
+            <td>{name}</td>
+            <td>{description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default WeaponTable;
+
+const Table = styled.table`
+  width: 100%;
+  text-align: center;
+  border-collapse: collapse;
+  tr {
+    border: 1px solid #efefef;
+  }
+
+  th {
+    padding: 8px;
+    font-size: 14px;
+    color: #6f6863;
+    background-color: #efeeee;
+  }
+
+  td {
+    padding: 10px 8px;
+    font-size: 14px;
+    color: #6f6863;
+  }
+`;
